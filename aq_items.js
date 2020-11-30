b831094ad1b555eed0884312de66462d68fa1b08
AlexStuffs <74549041+AlexStuffs@users.noreply.github.com>
2020-11-30 13:29:26 +0100

    Add files via upload

---
+++ aq_items.js
@@ -0,0 +1,302 @@
+var items = [
+  {
+    "number": "QA01",
+    "Prompt": "Je préfère réaliser des activités avec d’autres personnes plutôt que seul(e).",
+    "Subscale": "Social Skills",
+    "Reverse": "Y"
+  },
+  {
+    "number": "QA02",
+    "Prompt": "Je préfère tout faire continuellement de la même manière.",
+    "Subscale": "Attention Switching",
+    "Reverse": "N"
+  },
+  {
+    "number": "QA03",
+    "Prompt": "Quand j’essaye d’imaginer quelque chose, il est très facile de m’en représenter une image mentalement.",
+    "Subscale": "Imagination",
+    "Reverse": "Y"
+  },
+  {
+    "number": "QA04",
+    "Prompt": "Je suis fréquemment tellement absorbé(e) par une chose que je perds tout le reste de vue.",
+    "Subscale": "Attention Switching",
+    "Reverse": "N"
+  },
+  {
+    "number": "QA05",
+    "Prompt": "Mon attention est souvent attirée par des bruits discrets que les autres ne remarquent pas.",
+    "Subscale": "Attention Detail",
+    "Reverse": "N"
+  },
+  {
+    "number": "QA06",
+    "Prompt": "Je fais habituellement attention aux numéros de plaques d’immatriculation ou à d’autres types d’informations de ce genre.",
+    "Subscale": "Attention Detail",
+    "Reverse": "N"
+  },
+  {
+    "number": "QA07",
+    "Prompt": "Les gens me disent souvent que ce que j’ai dit était impoli, même quand je pense moi que c’était poli.",
+    "Subscale": "Communication",
+    "Reverse": "N"
+  },
+  {
+    "number": "QA08",
+    "Prompt": "Quand je lis une histoire, je peux facilement imaginer à quoi les personnages pourraient ressembler.",
+    "Subscale": "Imagination",
+    "Reverse": "Y"
+  },
+  {
+    "number": "QA09",
+    "Prompt": "Je suis fasciné(e) par les dates.",
+    "Subscale": "Attention Detail",
+    "Reverse": "N"
+  },
+  {
+    "number": "QA10",
+    "Prompt": "Au sein d’un groupe, je peux facilement suivre les conversations de plusieurs personnes à la fois.",
+    "Subscale": "Attention Switching",
+    "Reverse": "Y"
+  },
+  {
+    "number": "QA11",
+    "Prompt": "Je trouve les situations de la vie en société faciles.",
+    "Subscale": "Social Skills",
+    "Reverse": "Y"
+  },
+  {
+    "number": "QA12",
+    "Prompt": "J’ai tendance à remarquer certains détails que les autres ne voient pas.",
+    "Subscale": "Attention Detail",
+    "Reverse": "N"
+  },
+  {
+    "number": "QA13",
+    "Prompt": "Je préfèrerais aller dans une bibliothèque plutôt qu’à une fête.",
+    "Subscale": "Social Skills",
+    "Reverse": "N"
+  },
+  {
+    "number": "QA14",
+    "Prompt": "Je trouve facile d’inventer des histoires.",
+    "Subscale": "Imagination",
+    "Reverse": "Y"
+  },
+  {
+    "number": "QA15",
+    "Prompt": "Je suis plus facilement attiré(e) par les gens que par les objets.",
+    "Subscale": "Social Skills",
+    "Reverse": "Y"
+  },
+  {
+    "number": "QA16",
+    "Prompt": "J’ai tendance à avoir des centres d’intérêt très importants.  Je me tracasse lorsque je ne peux m’y consacrer.",
+    "Subscale": "Attention Switching",
+    "Reverse": "N"
+  },
+  {
+    "number": "QA17",
+    "Prompt": "J’apprécie le bavardage en société.",
+    "Subscale": "Communication",
+    "Reverse": "Y"
+  },
+  {
+    "number": "QA18",
+    "Prompt": "Quand je parle, il n’est pas toujours facile pour les autres de placer un mot.",
+    "Subscale": "Communication",
+    "Reverse": "N"
+  },
+  {
+    "number": "QA19",
+    "Prompt": "Je suis fasciné(e) par les chiffres.",
+    "Subscale": "Attention Detail",
+    "Reverse": "N"
+  },
+  {
+    "number": "QA20",
+    "Prompt": "Quand je lis une histoire, je trouve qu’il est difficile de me représenter les intentions des personnages.",
+    "Subscale": "Imagination",
+    "Reverse": "N"
+  },
+  {
+    "number": "QA21",
+    "Prompt": "Je n’aime pas particulièrement lire des romans.",
+    "Subscale": "Imagination",
+    "Reverse": "N"
+  },
+  {
+    "number": "QA22",
+    "Prompt": "Je trouve qu’il est difficile de se faire de nouveaux amis.",
+    "Subscale": "Social Skills",
+    "Reverse": "N"
+  },
+  {
+    "number": "QA23",
+    "Prompt": "Je remarque sans cesse des schémas réguliers dans les choses qui m’entourent.",
+    "Subscale": "Attention Detail",
+    "Reverse": "N"
+  },
+  {
+    "number": "QA24",
+    "Prompt": "Je préfèrerais aller au théâtre qu’au musée.",
+    "Subscale": "Imagination",
+    "Reverse": "Y"
+  },
+  {
+    "number": "QA25",
+    "Prompt": "Cela ne me dérange pas si mes habitudes quotidiennes sont perturbées.",
+    "Subscale": "Attention Switching",
+    "Reverse": "Y"
+  },
+  {
+    "number": "QA26",
+    "Prompt": "Je remarque souvent que je ne sais pas comment entretenir une conversation.",
+    "Subscale": "Communication",
+    "Reverse": "N"
+  },
+  {
+    "number": "QA27",
+    "Prompt": "Je trouve qu’il est facile de « lire entre les lignes » lorsque quelqu’un me parle.",
+    "Subscale": "Communication",
+    "Reverse": "Y"
+  },
+  {
+    "number": "QA28",
+    "Prompt": "Je me concentre habituellement plus sur l’ensemble d’une image que sur les petits détails de celle-ci.",
+    "Subscale": "Attention Detail",
+    "Reverse": "Y"
+  },
+  {
+    "number": "QA29",
+    "Prompt": "Je ne suis pas très doué(e) pour me souvenir des numéros de téléphone.",
+    "Subscale": "Attention Detail",
+    "Reverse": "Y"
+  },
+  {
+    "number": "QA30",
+    "Prompt": "Je ne remarque habituellement pas les petits changements dans une situation ou dans l’apparence de quelqu’un.",
+    "Subscale": "Attention Detail",
+    "Reverse": "Y"
+  },
+  {
+    "number": "QA31",
+    "Prompt": "Je sais m’en rendre compte quand mon interlocuteur s’ennuie.",
+    "Subscale": "Communication",
+    "Reverse": "Y"
+  },
+  {
+    "number": "QA32",
+    "Prompt": "Je trouve qu’il est facile de faire plus d’une chose à la fois.",
+    "Subscale": "Attention Switching",
+    "Reverse": "Y"
+  },
+  {
+    "number": "QA33",
+    "Prompt": "Quand je parle au téléphone, je ne suis pas sûr(e) de savoir quand c’est à mon tour de parler.",
+    "Subscale": "Communication",
+    "Reverse": "N"
+  },
+  {
+    "number": "QA34",
+    "Prompt": "J’aime faire les choses de manière spontanée.",
+    "Subscale": "Attention Switching",
+    "Reverse": "Y"
+  },
+  {
+    "number": "QA35",
+    "Prompt": "Je suis souvent le(la) dernier(ère) à comprendre le sens d’une blague.",
+    "Subscale": "Communication",
+    "Reverse": "N"
+  },
+  {
+    "number": "QA36",
+    "Prompt": "Je trouve qu’il est facile de décoder ce que les autres pensent ou ressentent juste en regardant leur visage.",
+    "Subscale": "Social Skills",
+    "Reverse": "Y"
+  },
+  {
+    "number": "QA37",
+    "Prompt": "Si je suis interrompu(e), je peux facilement revenir à ce que j’étais en train de faire.",
+    "Subscale": "Attention Switching",
+    "Reverse": "Y"
+  },
+  {
+    "number": "QA38",
+    "Prompt": "Je suis doué(e) pour le bavardage en société.",
+    "Subscale": "Communication",
+    "Reverse": "Y"
+  },
+  {
+    "number": "QA39",
+    "Prompt": "Les gens me disent souvent que répète continuellement les mêmes choses.",
+    "Subscale": "Communication",
+    "Reverse": "N"
+  },
+  {
+    "number": "QA40",
+    "Prompt": "Quand j’étais enfant, j’aimais habituellement jouer à des jeux de rôle avec les autres.",
+    "Subscale": "Imagination",
+    "Reverse": "Y"
+  },
+  {
+    "number": "QA41",
+    "Prompt": "J’aime collectionner des informations sur des catégories de choses (types de voitures, d’oiseaux, de trains, de plantes, ...).",
+    "Subscale": "Imagination",
+    "Reverse": "N"
+  },
+  {
+    "number": "QA42",
+    "Prompt": "Je trouve qu’il est difficile de s’imaginer dans la peau d’un autre.",
+    "Subscale": "Imagination",
+    "Reverse": "N"
+  },
+  {
+    "number": "QA43",
+    "Prompt": "J’aime planifier avec soin toute activité à laquelle je participe.",
+    "Subscale": "Attention Switching",
+    "Reverse": "N"
+  },
+  {
+    "number": "QA44",
+    "Prompt": "J’aime les événements sociaux.",
+    "Subscale": "Social Skills",
+    "Reverse": "Y"
+  },
+  {
+    "number": "QA45",
+    "Prompt": "Je trouve qu’il est difficile de décoder les intentions des autres.",
+    "Subscale": "Social Skills",
+    "Reverse": "N"
+  },
+  {
+    "number": "QA46",
+    "Prompt": "Les nouvelles situations me rendent anxieux(se).",
+    "Subscale": "Attention Switching",
+    "Reverse": "N"
+  },
+  {
+    "number": "QA47",
+    "Prompt": "J’aime rencontrer de nouvelles personnes.",
+    "Subscale": "Social Skills",
+    "Reverse": "Y"
+  },
+  {
+    "number": "QA48",
+    "Prompt": "Je suis une personne qui a le sens de la diplomatie.",
+    "Subscale": "Social Skills",
+    "Reverse": "Y"
+  },
+  {
+    "number": "QA49",
+    "Prompt": "Je ne suis pas très doué(e) pour me souvenir des dates de naissance des gens.",
+    "Subscale": "Attention Detail",
+    "Reverse": "Y"
+  },
+  {
+    "number": "QA50",
+    "Prompt": "Je trouve qu’il est très facile de jouer à des jeux de rôle avec des enfants.",
+    "Subscale": "Imagination",
+    "Reverse": "Y"
+  }
+]
---
+++ aq_items_short.js
@@ -0,0 +1,14 @@
+var items = [
+  {
+    "number": 1,
+    "Prompt": "(1)I prefer to do things with others rather than on my own",
+    "Subscale": "Social Skills",
+    "Reverse": "Y"
+  },
+  {
+    "number": 2,
+    "Prompt": "(2)I prefer to do things the same way over and over again",
+    "Subscale": "Attention Switching",
+    "Reverse": "N"
+  }
+]
